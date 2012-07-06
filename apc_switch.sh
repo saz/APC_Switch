0e41ab7b0749927f6eca7c6a8d6fc4440fbd04f8
Steffen Zieger <me@saz.sh>
2012-07-06 19:19:18 +0200

    fix usage

--- apc_switch.sh
+++ apc_switch.sh
@@ -3,7 +3,12 @@
 # Author: Steffen Zieger <me@saz.sh>
 # License: GPLv3
 # URL: https://github.com/saz/APC_Switch
-# Version: 1.0
+# Version: 1.1
+
+usage() {
+	echo "Usage: $0 <host> <community> <outlet> <on|off>"
+	exit 1
+}
 
 if [ $# -ne 4 ]; then
 	usage
@@ -14,12 +19,6 @@ community="$2"
 outlet=$3
 action=$4
 snmpVer=1
-
-usage()
-{
-	echo "Usage: $0 <outlet> <on|off>"
-	exit 1
-}
 
 if [ ! -x `which snmpset` ]; then
 	echo "snmpset not found. Exiting!"
